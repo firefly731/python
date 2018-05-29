b990763c45645642bc56c916da091854aacba2a7
Jerome DUMOULIN <jerome.dumoulin@passman.fr>
2018-05-29 14:42:44 +0200

    remplacement script test ports

--- snmp switch HP/snmp-test-ports.py
+++ snmp switch HP/snmp-test-ports.py
@@ -5,6 +5,15 @@ from pysnmp.hlapi import *
 from pysnmp.entity.rfc3413.oneliner import cmdgen
 import re
 
+OID_product  = '1.3.6.1.4.1.11.2.36.1.1.2.5.0'
+OID_hostname = '1.3.6.1.2.1.1.5.0'
+OID_serial = '1.3.6.1.4.1.11.2.36.1.1.2.9.0'
+OID_firmware = '1.3.6.1.4.1.11.2.36.1.1.5.1.1.11.1'
+OID_description = '1.3.6.1.2.1.47.1.1.1.1.2.1'
+OID_ports_up = '1.3.6.1.2.1.2.2.1.8'
+OID_ports_poe = '1.3.6.1.4.1.11.2.14.11.1.9.1.1.1.3'
+OID_MAC = '1.3.6.1.2.1.17.4.3.1.2'
+
 #SNMPGET
 def get_value(IP, PORT, COMMUNITY, OID_ref):
     errorIndication, errorStatus, errorIndex, varBinds = cmdgen.CommandGenerator().getCmd(
@@ -17,7 +26,6 @@ def get_value(IP, PORT, COMMUNITY, OID_ref):
     else:
         return errorIndication
 
-#SNMPWALK
 def walk_value(IP, PORT, COMMUNITY, OID_ref):
     tableau = []
     for (errorIndication,
@@ -41,59 +49,75 @@ def walk_value(IP, PORT, COMMUNITY, OID_ref):
                 tableau.append(str(varBind))
     return tableau
 
-IP = '172.20.1.36'
+Switch_HP = dict()
+# liste switch (modèle, nb_ports, gigabit, poe)
+Switch_HP = {"J9776A":(2530, 24, True, False),
+            "J9773A":(2530, 24, True, True),
+            "J9779A":(2530, 24, False, True),
+            "J9299A":(2520, 24, True, True),
+            "J9138A":(2520, 24, False, True),
+            "J9772A":(2530, 48, True, True),
+            "J9781A":(2530, 48, False, False),
+            "J9778A":(2530, 48, False, True),
+            "J9137A":(2520, 8, False, True),
+            "J9298A":(2520, 8, True, True),
+            "J9783A":(2530, 8, False, False),
+            "J9780A":(2530, 8, False, True),
+            "J9777A":(2530, 8, True, False),
+            "J9774A":(2530, 8, True, True)}
+
 PORT = 16101
-COMMUNITY = 'uds7wVvB'
-OID_hostname = '1.3.6.1.2.1.1.5.0'
-OID_serial = '1.3.6.1.4.1.11.2.36.1.1.2.9.0'
-OID_firmware = '1.3.6.1.4.1.11.2.36.1.1.5.1.1.11.1'
-OID_description = '1.3.6.1.2.1.47.1.1.1.1.2.1'
-OID_ports_up = '1.3.6.1.2.1.2.2.1.8'
-OID_ports_poe = '1.3.6.1.4.1.11.2.14.11.1.9.1.1.1.3'
 
-Description = get_value(IP, PORT, COMMUNITY, OID_description).split()
-#On enlève la valeur 'Switch' dans la liste pour avoir la même liste avec les 2520 et les 2530
+IP = input("Veuillez saisir l'IP : ")
+while re.search(r'^(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])\.(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])\.(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])\.(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])$', IP) is None:
+    IP = input("Veuillez saisir une IP valide : ")
+
+COMMUNITY = input("Veuillez saisir la communauté SNMP : ")
+
+NB_SWITCH = input("Veuillez saisir le nombre de SWITCH : ")
+while re.search(r'^[1-9][0-9]?$', NB_SWITCH) is None:
+    NB_SWITCH = input("Veuillez saisir un nombre valide : ")
+
+NB_SWITCH = int(NB_SWITCH)
+
+PORT_MAC = walk_value(IP, PORT, COMMUNITY, OID_MAC)
 """
-Exemple de valeurs récupérées :
-HP J9776A 2530-24G Switch (Formerly ProCurve)
-HP J9773A 2530-24G-PoEP Switch (Formerly ProCurve)
-HP J9779A 2530-24-PoEP Switch (Formerly ProCurve)
-HP J9299A Switch 2520G-24-PoE (Formerly ProCurve)
-HP J9138A Switch 2520-24-PoE (Formerly ProCurve)
-HP J9772A 2530-48G-PoEP Switch (Formerly ProCurve)
+recup
+SNMPv2-SMI::mib-2.17.4.3.1.2.232.57.53.241.205.183 = 2', 'SNMPv2-SMI::mib-2.17.4.3.1.2.232.57.53.241.205.185 = 4'
 """
+PORT_LINK = walk_value(IP, PORT, COMMUNITY, OID_ports_up)
+"""
+recup
+iso.3.6.1.2.1.2.2.1.8.1 = INTEGER: 1 (UP)
+iso.3.6.1.2.1.2.2.1.8.2 = INTEGER: 1
+iso.3.6.1.2.1.2.2.1.8.3 = INTEGER: 1
+iso.3.6.1.2.1.2.2.1.8.4 = INTEGER: 1
+iso.3.6.1.2.1.2.2.1.8.5 = INTEGER: 2 (DOWN)
+iso.3.6.1.2.1.2.2.1.8.6 = INTEGER: 2
+iso.3.6.1.2.1.2.2.1.8.7 = INTEGER: 2
+iso.3.6.1.2.1.2.2.1.8.8 = INTEGER: 2
+"""
+PORT_POE = walk_value(IP, PORT, COMMUNITY, OID_ports_poe)
+"""
+recup
+iso.3.6.1.4.1.11.2.14.11.1.9.1.1.1.3.1.1 = INTEGER: 5301 (POE)
+iso.3.6.1.4.1.11.2.14.11.1.9.1.1.1.3.1.2 = INTEGER: 5385
+iso.3.6.1.4.1.11.2.14.11.1.9.1.1.1.3.1.3 = INTEGER: 5257
+iso.3.6.1.4.1.11.2.14.11.1.9.1.1.1.3.1.4 = INTEGER: 5222
+iso.3.6.1.4.1.11.2.14.11.1.9.1.1.1.3.1.5 = INTEGER: 0 (NO POE)
+iso.3.6.1.4.1.11.2.14.11.1.9.1.1.1.3.1.6 = INTEGER: 0
 
-Description.remove('Switch')
+"""
+while NB_SWITCH != 0:
+    Switch_key = get_value(IP, PORT, COMMUNITY, OID_product)
+    PORT += 1
+    NB_SWITCH -= 1
+    if "J9" not in str(Switch_key):
+        print("Une erreur s'est produite : ")
+        print(Switch_key)
+    else:
+        NB_PORT = Switch_HP[Switch_key][1]
+            
 
-Marque, Produit, Modele, *Junk = Description
-
-#Avec le modèle on récupère le nombre de ports et si le switch est POE
-NbPorts = int("".join(re.findall(r'[0-9]', Modele.split('-')[1])))
-if 'PoE' in Modele:
-    POE = True
-else:
-    POE = False
-
-Serial = get_value(IP, PORT, COMMUNITY, OID_serial)
-Firmware = get_value(IP, PORT, COMMUNITY, OID_firmware)
-Hostname = get_value(IP, PORT, COMMUNITY, OID_hostname)
-
-#On récupère la liste du statut des ports UP ou DOWN, hors ports d'uplink
-PortUP = walk_value(IP, PORT, COMMUNITY, OID_ports_up)
-NbPortsUP = 0
-NbPortsDOWN = 0
-for i in PortUP:
-    if NbPorts > 0:
-        if int(i.split()[-1]) == 1:
-            NbPortsUP += 1
-        else:
-            NbPortsDOWN += 1
-    NbPorts -= 1
-
-#On récupère la liste des ports qui délivrent du POE
-PortPOE = walk_value(IP, PORT, COMMUNITY, OID_ports_poe)
-NbPortsPOE = 0
-for i in PortPOE:
-    if int(i.split()[-1]) != 0:
-        NbPortsPOE += 1
-print(NbPortsPOE)
+# mac table to port : 1.3.6.1.2.1.17.4.3.1.2
+# mac table hexa : 1.3.6.1.2.1.17.4.3.1.1
